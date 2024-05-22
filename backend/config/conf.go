65344e4f56e593f9d157c306c8fb3749d63e8956
TerenceCui <2925780345@qq.com>
2024-05-23 00:09:43 +0800

    Add Config struct and Init function

--- backend/config/conf.go
+++ backend/config/conf.go
@@ -1,1 +1,41 @@
 package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+var ElemeiConfig Config
+
+type Config struct {
+	MySQLConfig MySQLConfig `json:"MySQLConfig"`
+	RedisConfig RedisConfig `json:"RedisConfig"`
+}
+
+type MySQLConfig struct {
+	Name string `json:"Name"`
+	Host string `json:"Host"`
+	User string `json:"User"`
+	Pass string `json:"Pass"`
+}
+
+type RedisConfig struct {
+	Addr string `json:"Addr"`
+	Pass string `json:"Pass"`
+}
+
+func Init(path string) error {
+	conf, err := os.ReadFile(path)
+	if err != nil {
+		log.Println(fmt.Errorf("read conf err: %v", err))
+		return err
+	}
+	err = json.Unmarshal(conf, &ElemeiConfig)
+	if err != nil {
+		log.Println(fmt.Errorf("conf bind json err: %v", err))
+		return err
+	}
+	return nil
+}
