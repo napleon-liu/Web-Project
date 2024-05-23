684cea8336463b063f5b7291b1d3053c7a45c3f7
xiuyi123 <154480066+xiuyi123@users.noreply.github.com>
2024-05-23 23:51:14 +0800

    Update dish.go

--- backend/service/dish.go
+++ backend/service/dish.go
@@ -1,1 +1,82 @@
 package service
+
+import (
+	"elemei/internal/model"
+	"elemei/internal/repository"
+	"elemei/internal/typ/req"
+	"elemei/internal/typ/resp"
+)
+
+func GetAllDishes() ([]resp.DishResp, error) {
+	dishes, err := repository.GetAllDishes()
+	if err != nil {
+		return nil, err
+	}
+
+	var dishResponses []resp.DishResp
+	for _, dish := range dishes {
+		dishResponses = append(dishResponses, resp.DishResp{
+			Id:          dish.ID,
+			Price:       dish.Price,
+			Name:        dish.Name,
+			PictureURL:  dish.PictureURL,
+			Description: dish.Description,
+		})
+	}
+
+	return dishResponses, nil
+}
+
+func UpdateDish(req req.UpdateDishReq) error {
+
+	dish, err := repository.GetDishByID(req.ID)
+	if err != nil {
+		return err
+	}
+
+	updates := map[string]interface{}{}
+
+	if req.Name != "" {
+		updates["name"] = req.Name
+	}
+	if req.Price != 0 {
+		updates["price"] = req.Price
+	}
+	if req.PictureURL != "" {
+		updates["picture_url"] = req.PictureURL
+	}
+	if req.Description != "" {
+		updates["description"] = req.Description
+	}
+
+	return repository.UpdateDish(dish, updates)
+}
+
+func CreateDish(req req.CreateDishReq) (uint, error) {
+	id, err := repository.CreateDish(model.Dish{
+		Price:       req.Price,
+		Name:        req.Name,
+		PictureURL:  req.PictureURL,
+		Description: req.Description,
+	})
+	return id, err
+}
+
+func DeleteDish(ids []uint) error {
+	return repository.DeleteDish(ids)
+}
+
+func GetDishDetail(id uint) (resp.DishResp, error) {
+	dish, err := repository.GetDishByID(id)
+	if err != nil {
+		return resp.DishResp{}, err
+	}
+
+	return resp.DishResp{
+		Id:          dish.ID,
+		Price:       dish.Price,
+		Name:        dish.Name,
+		PictureURL:  dish.PictureURL,
+		Description: dish.Description,
+	}, nil
+}
