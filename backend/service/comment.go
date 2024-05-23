45de0787af307d427f06a8f1e0baa5307250f14d
xiuyi123 <154480066+xiuyi123@users.noreply.github.com>
2024-05-23 23:49:35 +0800

    Update comment.go

--- backend/service/comment.go
+++ backend/service/comment.go
@@ -1,1 +1,23 @@
 package service
+
+import (
+	"elemei/internal/model"
+	"elemei/internal/repository"
+	"elemei/internal/typ/req"
+	"elemei/internal/typ/resp"
+)
+
+func CreateComment(req req.CreateCommentReq) error {
+	var comment = model.Comment{
+		Content:    req.Content,
+		Rating:     req.Rating,
+		OrderID:    req.OrderID,
+		CustomerID: req.CustomerID,
+	}
+	return repository.CreateComment(comment)
+}
+
+func GetCommentList(commentReq req.GetCommentReq) (resp.GetCommentResp, error) {
+	orderID := commentReq.OrderID
+	return repository.GetCommentByOrderID(orderID)
+}
